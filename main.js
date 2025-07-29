745660a4ae33816f182a48c01f6188efeac6e0b1
a7medElkholy <a7medeelkholy@gmail.com>
2025-07-29 23:20:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach(link => {
+    link.addEventListener('click', function () {
+      links.forEach(l => l.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+
+  AOS.init();

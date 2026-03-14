72bfe1a72e91ab531dab9d50422d6be4fede646b
a7med ELkholy <a7medeelkholy@gmail.com>
2026-03-14 05:33:28 +0200

    new web

--- main.js
+++ main.js
@@ -8,3 +8,17 @@
   });
 
   AOS.init();
+
+
+//   const backgrounds = [
+//     "url('img/back-ground.jpg')",
+//     "url('img/new.jpg')",
+// ];
+
+// let index = 0;
+// const element = document.querySelector(".home");
+ 
+// setInterval(() => {
+//     index = (index + 1) % backgrounds.length;
+//     element.style.backgroundImage = backgrounds[index];
+// }, 10000); // 30000 ms = 30 seconds
